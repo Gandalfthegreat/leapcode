fe52daf189472cc5071d089dd9a6d90e568852f3
Yi Ying <yingyitc@gmail.com>
2020-06-20 12:00:36 +0800

    First try, wrong answer

---
+++ leapcode1/20-06-20-binary-search/378-kth-smallest-element-in-a-sorted-matrix.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(matrix, k) {
+    row_len = matrix.length;
+    let low = matrix[0][0];
+    let high = matrix[row_len-1][row_len-1];
+    while (low < high) {
+        let mid = low + (high - low) / 2;
+        let num = lesser(matrix, mid);
+        if (num < k) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    return low;
+};
+
+// return number of elements lesser than a in matrix
+function lesser(matrix, a) {
+    let i = 0, j = 0;
+    let num = 0;
+    row_len = matrix.length;
+    while (i < row_len) {
+        while (j < row_len) {
+            if (matrix[i][j] <= a) {
+                num++;
+            } else {
+                j = 0;
+                break;
+            }
+            j++;
+        }
+        i++;
+    }
+    return num;
+}
